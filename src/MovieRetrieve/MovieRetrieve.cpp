7ab0b309a1ab06c623c2c7e12358538841dc08b6
BlackCloud37 <44435569+BlackCloud37@users.noreply.github.com>
2019-12-15 18:09:34 +0800

    final version 1.1

--- src/MovieRetrieve/MovieRetrieve.cpp
+++ src/MovieRetrieve/MovieRetrieve.cpp
@@ -17,46 +17,19 @@
 #include "Segmenter.h"
 #include "DocList.h"
 #include "Solve.h"
-// 控制台显示乱码纠正
-void init() {
-	system("chcp 65001"); //设置字符集 （使用SetConsoleCP(65001)设置无效，原因未知）
-
-	CONSOLE_FONT_INFOEX info = { 0 }; // 以下设置字体
-	info.cbSize = sizeof(info);
-	info.dwFontSize.Y = 16; // leave X as zero
-	info.FontWeight = FW_NORMAL;
-	wcscpy(info.FaceName, L"Consolas");
-	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), NULL, &info);
-}
 
 
 int main()
 {
 	Solve s;
-	//init();
 	clock_t start, end;
 	start = clock();
-	//testAVL();
-	//testDocList();
-	//testSeg();
-	//solve();
-	//testPunc();
-	//testAVLandDoclist();
 	s.extractWordsToFile();
 	s.createAVLDictionary();
 	s.createRecommendTree();
 	s.queryToFile();
 	s.recommendToFile();
 	s.generateHtml();
-	
-	//std::cout << "\n";
-	//s.recommend(2).printList();
-	//s.queryToFile();
-	//testListIter();
 	end = clock();
 	std::cout << "Time Used: " << (end - start) / 1000 << "s\n";
 }
-
-
-
-//todo: 修改getname 去重
